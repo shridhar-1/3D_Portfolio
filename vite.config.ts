b96561b27ca9a6730d726ed5bd35685ff22af019
shridhar-1 <madivalshridhar1@gmail.com>
2026-08-09 19:35:09 +0530

    Fix folder structure and push local project

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,15 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/3D_Portfolio/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile(),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
